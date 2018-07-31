0ac9ae621949180a94a08a3924269a7558a8a3ae
Amandar8 <amandaruth08@gmail.com>
2018-07-31 13:37:55 -0500

    first commit

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -1,10 +1,36 @@
+
 'use strict';
 
 /* eslint-env browser */
 
 (() => {
+
     function populateList(results) {
+        let userList = document.getElementById('z-user-list')
         console.log(results); // eslint-disable-line no-console
+        for (let i = 0; i<results.length; i++){
+            let user = document.createElement('li');
+            user.classList.add('user');
+
+            let userPhoto = document.createElement('img');
+            userPhoto.classList.add('user-photo');
+            userPhoto.setAttribute('src', results[i].picture.medium);
+
+            let userName = document.createElement('div');
+            userName.classList.add("user-name");
+            userName.innerHTML = results[i].name.first + " " + results[i].name.last;
+
+            let userLocation = document.createElement('div');
+            userLocation.classList.add("user-location");
+            userLocation.innerHTML = results[i].location.city + " " + results[i].location.state;
+       
+            let userEmail = document.createElement('div');
+            userEmail.classList.add("user-email");
+            userEmail.innerHTML = results[i].email
+
+            user.append(userPhoto, userName, userLocation, userEmail);
+            userList.append(user);
+        }
     }
 
     function init() {
@@ -15,3 +41,4 @@
 
     document.addEventListener('DOMContentLoaded', init);
 })();
+
