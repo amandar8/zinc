42ffe2af9f9216888006aa3ae25357a5f4bfec3b
Amandar8 <amandaruth08@gmail.com>
2018-08-01 15:29:51 -0500

    creating branches for each part, first branch

--- 01-Templates/part4.js
+++ 01-Templates/part4.js
@@ -1,40 +1,33 @@
+
 'use strict';
 
 /* eslint-env browser */
 
+
 (() => {
 
-  function renderTemplate(userTemplate, data) {
-    let userList = document.getElementById('z-user-list');
+    function renderTemplate(template, data) {
+        fetch(`${template}.html`)
+            .then(template => template.text())
+            .then((template) => {
+                let regex = /{{\s*([\w.]+)\s*}}/g;
 
-    console.log(data);
+                data.forEach((user) => {
+                    let renderTemplate = template.replace(regex, (match, captured) => {
+                        let arr = captured.split('.');
+                        return arr.reduce((acc, curr) => acc[curr], user);
+                    });
+                    document.getElementById('z-user-list').insertAdjacentHTML('beforeend', renderTemplate);
+                });
+            });
+    };
 
-    let regex = /{{\s*([\w.]+)\s*}}/gm;
+    function init() {
+        fetch('https://randomuser.me/api/?results=5')
+            .then(res => res.json())
+            .then(data => renderTemplate('user', data.results));
+    }
 
-    data.forEach(user =>
-      userList.insertAdjacentHTML('beforeend', userTemplate.replace(regex, (match, captured) =>
-        captured.split('.').reduce((acc, curr) =>
-          acc[curr], user)))
-    );
-  }
-
-  function init() {
-    fetch('https://randomuser.me/api/?results=5')
-      .then(res => res.json())
-      .then(json => {
-
-        let userTemplate = `
-      <li class="user">
-        <img class="user-photo" src="{{ picture.thumbnail }}" alt="Photo of {{ name.first }} {{ name.last }}">
-        <div class="user-name">{{ name.first }} {{ name.last }}</div>
-        <div class="user-location">{{ location.city }}, {{ location.state }}</div>
-        <div class="user-email">{{ email }}</div>
-      </li>`;
-
-
-        renderTemplate(userTemplate, json.results);
-      })
-  }
-
-  document.addEventListener('DOMContentLoaded', init);
+    document.addEventListener('DOMContentLoaded', init);
+    
 })();
