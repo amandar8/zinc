96951ff96634f312da673899e4bf9f820040dc0c
Amandar8 <amandaruth08@gmail.com>
2018-08-01 16:00:56 -0500

    branch part1.5

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -4,51 +4,26 @@
 
 (() => {
 
-    function renderTemplate(userTemplate, data) { //function takes in two arguments, a template string and an object
-        let userList = document.getElementById('z-user-list');
+    function populateList(results) {
+        let userList = document.getElementById('z-user-list'); //grab element of z-user-list
+        console.log(results); // eslint-disable-line no-console
+        for (let i = 0; i < results.length; i++) { //loops through results
 
-        console.log(data);
+            userList.insertAdjacentHTML('beforeend', `
+    <li class="user">
+      <img class="user-photo" src="${results[i].picture.medium}" alt="">
+      <div class="user-name">${results[i].name.first} ${results[i].name.last}</div>
+      <div class="user-location">${results[i].location.city}, ${results[i].location.state}</div>
+      <div class="user-email">${results[i].email}</div>
+    </li>`);
 
-        let regex = /{{\s(.*?)\s}}/gm;
-
-        let dataMap = data.map(user => ({
-
-            photo: user.picture.thumbnail,
-            firstName: user.name.first,
-            lastName: user.name.last,
-            city: user.location.city,
-            state: user.location.state,
-            email: user.email
-
-
-            // return userData
-        }));
-
-        dataMap.forEach(user => { //loops through our dataMap
-            let userString = userTemplate.replace(regex, (match, captured) => { //goes through userTemplate and looks for regex to match and capture and insert info
-                return user[captured];
-
-            })
-            userList.insertAdjacentHTML('afterend', userString);
-        });
+        }
     }
 
     function init() {
         fetch('https://randomuser.me/api/?results=5')
             .then(res => res.json())
-            .then(json => {
-
-                const userTemplate = `
-      <li class="user">
-        <img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
-        <div class="user-name">{{ firstName }} {{ lastName }}</div>
-        <div class="user-location">{{ city }}, {{ state }}</div>
-        <div class="user-email">{{ email }}</div>
-      </li>`;
-
-
-                renderTemplate(userTemplate, json.results);
-            })
+            .then(json => populateList(json.results));
     }
 
     document.addEventListener('DOMContentLoaded', init);
