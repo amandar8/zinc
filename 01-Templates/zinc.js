ba17ff2c59b7b41df121778e0423af27cdea0bfd
Amandar8 <amandaruth08@gmail.com>
2018-08-01 15:53:10 -0500

    branch part2

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -1,45 +1,55 @@
-
 'use strict';
 
 /* eslint-env browser */
 
 (() => {
 
-    function populateList(results) {
-        let userList = document.getElementById('z-user-list'); //grab element of z-user-list
-        console.log(results); // eslint-disable-line no-console
-        for (let i = 0; i<results.length; i++){ //loops through results
+    function renderTemplate(userTemplate, data) { //function takes in two arguments, a template string and an object
+        let userList = document.getElementById('z-user-list');
 
-            let user = document.createElement('li'); //creates li element
-            user.classList.add('user'); //creates class named user
+        console.log(data);
 
-            let userPhoto = document.createElement('img');
-            userPhoto.classList.add('user-photo');
-            userPhoto.setAttribute('src', results[i].picture.medium);
+        let regex = /{{\s(.*?)\s}}/gm;
 
-            let userName = document.createElement('div');
-            userName.classList.add("user-name");
-            userName.innerHTML = results[i].name.first + " " + results[i].name.last;
+        let dataMap = data.map(user => ({
 
-            let userLocation = document.createElement('div');
-            userLocation.classList.add("user-location");
-            userLocation.innerHTML = results[i].location.city + " " + results[i].location.state;
-       
-            let userEmail = document.createElement('div');
-            userEmail.classList.add("user-email");
-            userEmail.innerHTML = results[i].email
+            photo: user.picture.thumbnail,
+            firstName: user.name.first,
+            lastName: user.name.last,
+            city: user.location.city,
+            state: user.location.state,
+            email: user.email
 
-            user.append(userPhoto, userName, userLocation, userEmail); //allows to add multiple elements to li
-            userList.append(user); //append user info to userList
-        }
+
+            // return userData
+        }));
+
+        dataMap.forEach(user => { //loops through our dataMap
+            let userString = userTemplate.replace(regex, (match, captured) => { //goes through userTemplate and looks for regex to match and capture and insert info
+                return user[captured];
+
+            })
+            userList.insertAdjacentHTML('afterend', userString);
+        });
     }
 
     function init() {
         fetch('https://randomuser.me/api/?results=5')
             .then(res => res.json())
-            .then(json => populateList(json.results));
+            .then(json => {
+
+                const userTemplate = `
+      <li class="user">
+        <img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
+        <div class="user-name">{{ firstName }} {{ lastName }}</div>
+        <div class="user-location">{{ city }}, {{ state }}</div>
+        <div class="user-email">{{ email }}</div>
+      </li>`;
+
+
+                renderTemplate(userTemplate, json.results);
+            })
     }
 
     document.addEventListener('DOMContentLoaded', init);
 })();
-
