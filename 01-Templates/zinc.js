e422ddcf14b7ff61af459c8060bc5abe3e53865e
Amandar8 <amandaruth08@gmail.com>
2018-08-02 10:58:29 -0500

    commiting

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -1,30 +1,48 @@
-'use strict';
+function renderTemplate2(userTemplate, data) {
+    let userList = document.getElementById("z-user-list");
+    let matchString = /{{\s*([\w.]+)\s*}}/gm;
+    console.log(data);
 
-/* eslint-env browser */
+    data.forEach(user => {
+        userList.insertAdjacentHTML('beforeend', userTemplate.replace(matchString, (match, captured) => {
+            return captured.split(".").reduce((acc, curr) => acc[curr], user)
+        }))
+    })
+}
 
-(() => {
+function renderTemplate(template, users) {
 
-    function populateList(results) {
-        let userList = document.getElementById('z-user-list'); //grab element of z-user-list
-        console.log(results); // eslint-disable-line no-console
-        for (let i = 0; i < results.length; i++) { //loops through results
+    fetch(`${template}.html`)
+        .then(template => template.text())
+        .then(template => {
+            let matchString = /{{\s*([\w.]+)\s*}}/g;
 
-            userList.insertAdjacentHTML('beforeend', `
-    <li class="user">
-      <img class="user-photo" src="${results[i].picture.medium}" alt="">
-      <div class="user-name">${results[i].name.first} ${results[i].name.last}</div>
-      <div class="user-location">${results[i].location.city}, ${results[i].location.state}</div>
-      <div class="user-email">${results[i].email}</div>
-    </li>`);
+            users.forEach((user) => {
 
-        }
-    }
+                document.getElementById("z-user-list").insertAdjacentHTML('beforeend', template.replace(matchString, (match, captured) => {
+                    let arr = captured.split('.');
+                    return arr.reduce((acc, curr) => acc[curr], user);
+                }))
+            });
+        });
+}
 
-    function init() {
-        fetch('https://randomuser.me/api/?results=5')
-            .then(res => res.json())
-            .then(json => populateList(json.results));
-    }
+function init() {
+    fetch('https://randomuser.me/api/?results=5')
+        .then(res => res.json())
+        .then(data => {
+            const userTemplate =
+                `<li class="user">
+                    <img class="user-photo" src="{{ picture.thumbnail }}" alt="Photo of {{ name.first }} {{ name.last }}">
+                    <div class="user-name">{{ name.first }} {{ name.last }}</div>
+                    <div class="user-location">{{ location.city }}, {{ location.state }}</div>
+                    <div class="user-email">{{ email }}</div>
+                </li>`;
 
-    document.addEventListener('DOMContentLoaded', init);
+            // renderTemplate2(userTemplate, data.results);
+            renderTemplate('user', data.results);
+        })
+}
+
+document.addEventListener('DOMContentLoaded', init);
 })();
